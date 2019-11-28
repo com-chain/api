06d663d071afc55ebcd59bb4ea89c09d0b239c15
Florian Dubath <florian.dubath@unige.ch>
2019-11-28 13:29:37 +0100

    simplify duplicated code

--- ecrecover_helper.php
+++ ecrecover_helper.php
@@ -7,37 +7,11 @@ require_once './Keccak.php';
 use kornrunner\Keccak;
 
 function personal_ecRecover($msg, $signed) {
-    $personal_prefix_msg = "\x19Ethereum Signed Message:\n". strlen($msg). $msg;
-    $hex = keccak256($personal_prefix_msg);
-    return ecRecover($hex, $signed);
+    return personal_ecRecoverPublic($msg, $signed)[0];
 }
 
 function ecRecover($hex, $signed) {
-    $rHex   = substr($signed, 2, 64);
-    $sHex   = substr($signed, 66, 64);
-    $vValue = hexdec(substr($signed, 130, 2));
-    $messageHex       = substr($hex, 2);
-    $messageByteArray = unpack('C*', hex2bin($messageHex));
-    $messageGmp       = gmp_init("0x" . $messageHex);
-    $r = $rHex;		//hex string without 0x
-    $s = $sHex; 	//hex string without 0x
-    $v = $vValue; 	//27 or 28
-
-    //with hex2bin it gives the same byte array as the javascript
-    $rByteArray = unpack('C*', hex2bin($r));
-    $sByteArray = unpack('C*', hex2bin($s));
-    $rGmp = gmp_init("0x" . $r);
-    $sGmp = gmp_init("0x" . $s);
-
-    $recovery = $v - 27;
-    if ($recovery !== 0 && $recovery !== 1) {
-        throw new Exception('Invalid signature v value');
-    }
-
-    $publicKey = Signature::recoverPublicKey($rGmp, $sGmp, $messageGmp, $recovery);
-    $publicKeyString = $publicKey["x"] . $publicKey["y"];
-
-    return '0x'. substr(keccak256(hex2bin($publicKeyString)), -40);
+    return ecRecoverPublic($hex, $signed)[0];
 }
 
 function personal_ecRecoverPublic($msg, $signed) {
